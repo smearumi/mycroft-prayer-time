2835ecf915cfc2fa3e6fc2fefc1a473378a2a274
S. M. Estiaque Ahmed <rumi@localhost.localdomain>
2019-10-05 05:35:57 +0600

    changes for testing.

--- __init__.py
+++ __init__.py
@@ -15,40 +15,40 @@
 import os
 import requests
 from subprocess import Popen
-from datetime import datetime
+from datetime import datetime, timedelta
 from adapt.intent import IntentBuilder
 from mycroft.util import get_cache_directory
 from mycroft.skills.core import intent_handler
 from mycroft.skills.common_play_skill import CommonPlaySkill, CPSMatchLevel
 
-direct_play_filetypes = [".mp3"]
-
 
 class PrayerTimeSkill(CommonPlaySkill):
     def __init__(self):
         super(PrayerTimeSkill, self).__init__(name="PrayerTimeSkill")
-
-        self.curl = None
-        self.now_playing = None
+        self.prayer_times = None
         self.STREAM = '{0}/stream.mp3'.format(
-                                    get_cache_directory("PrayerTimeSkill"))
+                                        get_cache_directory("PrayerTimeSkill"))
+
+        self.first_time_event_flag = True
+
+    def initialize(self):
+        self.handle_start_intent("Initial")
 
     def CPS_match_query_phrase(self, phrase):
-        self.log.error("CPS MATCH")
-        phrase = ' '.join(phrase.lower().split())
-
-        self.log.error(phrase)
-
-        if self.voc_match(phrase, "PrayerTime"):
-            return ("PrayerTime", CPSMatchLevel.TITLE)
+        pass
 
     def CPS_start(self, phrase, data):
-        self.log.error("CPS START")
-        self.handle_start_intent()
+        pass
 
     @intent_handler(IntentBuilder("StartPrayerTimeIntent").require("Start")
                     .require("PrayerTime"))
     def handle_start_intent(self, message):
+        if not self.first_time_event_flag:
+            # TODO: dialog for stopping first.
+            return
+
+        self.curl = None
+
         try:
             self.city = self.location['city']['state']['name']
             self.country = self.location['city']['state']['country']['name']
@@ -65,74 +65,100 @@ class PrayerTimeSkill(CommonPlaySkill):
                                                             self.adhan_url,
                                                             adhan)
 
-            self._schedule_event()
-
-        except Exception as e:
+        except Exception:
             self.speak_dialog("settings.error")
-            self.log.error(e)
+
+        start_event = datetime.now()
+        next_event = start_event.replace(
+                                    hour=0,
+                                    minute=1,
+                                    second=0) + timedelta(days=1)
+        self.interval = int((next_event - start_event).total_seconds())
+
+        # self._schedule_event()
+        self.cancel_scheduled_event(name="PrayerTime")
+
+        self.schedule_repeating_event(
+                                    self._schedule_event,
+                                    start_event,
+                                    self.interval,
+                                    name="PrayerTime")
+
+        # self.log.error(self.get_scheduled_event_status("PrayerTime"))
 
     @intent_handler(IntentBuilder("StopPrayerTimeIntent").require("Stop")
                     .require("PrayerTime"))
     def handle_stop_intent(self, message):
-        try:
-            self.log.error(self.get_scheduled_event_status("PrayerTime"))
-            self.cancel_scheduled_event(name="PrayerTime")
+        self.first_time_event_flag = True
+        self.cancel_scheduled_event(name="PrayerTime")
 
-        except Exception:
-            pass
+        if self.prayer_times:
+            for key, value in self.prayer_times.items():
+                self.cancel_scheduled_event(name="PrayerTime{0}".format(key))
+
+        self.stop()
 
     def _schedule_event(self):
-        try:
-            self.remove_event(name="PrayerTime")
-            self.log.error(self.get_scheduled_event_status(name="PrayerTime"))
-            self.cancel_scheduled_event(name="PrayerTime")
+        self.log.error("_schedule_event")
+        self.log.error(self.get_scheduled_event_status("PrayerTime"))
+        self.log.error(self.interval)
 
-        except Exception as e:
-            self.log.error("ERRORRRR")
-            self.log.error(e)
-            pass
+        if not self.first_time_event_flag and not self.interval == 86400:
+            self.interval = 86400
+            self.cancel_scheduled_event(name="PrayerTime")
+            self.schedule_repeating_event(
+                                        self._schedule_event,
+                                        0,
+                                        self.interval,
+                                        name="PrayerTime")
 
         prayer_times = self.get_api_data()
 
         if prayer_times:
             self.prayer_times = prayer_times
 
+        if not self.prayer_times:
+            self.handle_stop_intent("Initial")
+            return
+
+        self.first_time_event_flag = False
+
         current_time = datetime.now()
 
         for key, value in self.prayer_times.items():
-            if current_time <= value:
-                break
+            self.cancel_scheduled_event(name="PrayerTime{0}".format(key))
 
-        self.log.error(value)
-        self.schedule_event(self.play_adhan, value, name="PrayerTime")
-        self.log.error(self.get_scheduled_event_status("PrayerTime"))
-        self.log.error(value)
+            if current_time < value:
+                self.schedule_event(
+                                self.play_adhan,
+                                value,
+                                name="PrayerTime{0}".format(key))
 
     def get_api_data(self):
         prayer_times = None
 
-        try:
-            api_url = "http://api.aladhan.com/v1/timingsByCity"
-            api_url = "{0}?city={1}&country={2}&method={3}".format(
-                                                                api_url,
-                                                                self.city,
-                                                                self.country,
-                                                                self.method)
+        # try:
+        #     api_url = "http://api.aladhan.com/v1/timingsByCity"
+        #     api_url = "{0}?city={1}&country={2}&method={3}".format(
+        #                                                         api_url,
+        #                                                         self.city,
+        #                                                         self.country,
+        #                                                         self.method)
 
-            api_headers = {'Content-Type': 'application/json',
-                           'Accept': 'application/json'}
+        #     api_headers = {'Content-Type': 'application/json',
+        #                    'Accept': 'application/json'}
 
-            response = requests.get(api_url, headers=api_headers)
+        #     response = requests.get(api_url, headers=api_headers)
 
-            if response.status_code == 200:
-                prayer_times = response.json()['data']['timings']
+        #     if response.status_code == 200:
+        #         prayer_times = response.json()['data']['timings']
 
-            else:
-                return None
+        #     else:
+        #         return None
 
-        except Exception as e:
-            self.log.error(e)
-            return None
+        # except Exception as e:
+        #     self.log.error(e)
+        #     return None
 
         # fajr = datetime.strptime(prayer_times['Fajr'], "%H:%M")
         # dhuhr = datetime.strptime(prayer_times['Dhuhr'], "%H:%M")
@@ -140,11 +166,11 @@ class PrayerTimeSkill(CommonPlaySkill):
         # maghrib = datetime.strptime(prayer_times['Maghrib'], "%H:%M")
         # isha = datetime.strptime(prayer_times['Isha'], "%H:%M")
 
-        fajr = datetime.strptime("19:40", "%H:%M")
-        dhuhr = datetime.strptime("19:41", "%H:%M")
-        asr = datetime.strptime("19:42", "%H:%M")
-        maghrib = datetime.strptime("19:43", "%H:%M")
-        isha = datetime.strptime("19:44", "%H:%M")
+        fajr = datetime.strptime("04:20", "%H:%M")
+        dhuhr = datetime.strptime("04:27", "%H:%M")
+        asr = datetime.strptime("05:10", "%H:%M")
+        maghrib = datetime.strptime("05:12", "%H:%M")
+        isha = datetime.strptime("05:15", "%H:%M")
 
         current_time = datetime.now()
 
@@ -173,15 +199,22 @@ class PrayerTimeSkill(CommonPlaySkill):
                         month=current_time.month,
                         day=current_time.day)
 
-        prayer_times = {'fajr': fajr,
-                        'dhuhr': dhuhr,
-                        'asr': asr,
-                        'maghrib': maghrib,
-                        'isha': isha}
+        prayer_times = {'Fajr': fajr,
+                        'Dhuhr': dhuhr,
+                        'Asar': asr,
+                        'Maghreeb': maghrib,
+                        'Isha': isha}
+
+        # prayer_times = {'Fajr': 5,
+        #                 'Dhuhr': 10,
+        #                 'Asar': 15,
+        #                 'Maghreeb': 20,
+        #                 'Isha': 25}
 
         return prayer_times
 
     def play_adhan(self):
+        self.log.error(self.get_scheduled_event_status("PrayerTime"))
         self.curl = None
 
         # if os.path.exists(self.STREAM):
@@ -196,18 +229,18 @@ class PrayerTimeSkill(CommonPlaySkill):
         #                 shell=True)
 
         # self.CPS_play(("file://" + self.STREAM, "audio/mpeg"))
-        self._schedule_event()
         self.log.error("Ajaan played!!!")
 
     def stop(self):
-        self.log.error("Stop")
+        self.log.error("Stop Prayer")
+
         if self.curl:
             try:
                 self.curl.kill()
                 self.curl.communicate()
 
-            except Exception as e:
-                self.log.error('Could not stop curl: {}'.format(repr(e)))
+            except Exception:
+                pass
 
             finally:
                 self.curl = None
